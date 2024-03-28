7e61c2a86835aceb15f69b1c62efa0e2217c1231
Ben Tracy <b.rob.tracy@gmail.com>
2024-03-28 13:29:53 -0400

    update readme, incorporate breeze auth/react scaffolding

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,22 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-    colors: {
-      primary: "#1d4ed8",
-      accent: "#f2ea07"
-    },
-  },
-  plugins: [],
-}
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.jsx',
+    ],
 
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [forms],
+};
